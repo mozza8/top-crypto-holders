e481273c676b08eead914cadd01150ce55bfe2f1
mozza8 <zan.mozoli@almamater.si>
2024-01-14 17:30:28 +0100

    Split code

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,24 @@
-import {
-  Box,
-  FormControl,
-  GlobalStyles,
-  Grid,
-  IconButton,
-  Input,
-  InputAdornment,
-  ListItemIcon,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
 
 // import components
 import HoldersTable from "./sections/HoldersTable";
 
 // import constants
-import { chainsIds } from "./constants/chains";
-import { apiKey } from "./constants/api";
 
 // import types
 import { HolderAddress, TokenData } from "./types/types";
 import { theme } from "./theme/theme";
-import { blue } from "@mui/material/colors";
+import Inputs from "./sections/Inputs";
 
 function App() {
-  const [selectedChain, setSelectedChain] = useState("1");
-  const [inputValue, setInputValue] = useState("");
   const [tokenHolders, setTokenHolders] = useState<HolderAddress[]>([]);
   const [tokenData, setTokenData] = useState<TokenData | null>();
 
-  const handleChainChange = (event: SelectChangeEvent) => {
-    setSelectedChain(event.target.value);
-  };
-
-  const handleAddress = (event: any) => {
-    setInputValue(event.target.value);
-  };
-
-  const sendTokenToApi = () => {
-    fetch(
-      `https://api.chainbase.online/v1/token/top-holders?chain_id=${selectedChain}&contract_address=${inputValue}&page=1&limit=100`,
-      {
-        method: "GET",
-        headers: {
-          "x-api-key": apiKey,
-          accept: "application/json",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => setTokenHolders(data.data))
-      .catch((error) => console.error(error));
-
-    fetch(
-      `https://api.chainbase.online/v1/token/metadata?chain_id=${selectedChain}&contract_address=${inputValue}`,
-      {
-        method: "GET",
-        headers: {
-          "x-api-key": apiKey,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => setTokenData(data.data))
-      .catch((error) => console.error(error));
-  };
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />
-      <GlobalStyles
-        styles={{
-          body: { backgroundColor: "background.default" },
-        }}
-      />
       <Grid
         container
         justifyContent={"center"}
@@ -89,61 +29,7 @@ function App() {
           TOP TOKEN HOLDERS
         </Typography>
       </Grid>
-      <Grid container sx={{ mt: 5 }}>
-        <Stack
-          sx={{ width: "200vh" }}
-          direction={"row"}
-          justifyContent={"space-around"}
-        >
-          <Stack direction={"column"} spacing={2}>
-            <Typography variant="h6">Choose chain:</Typography>
-            <FormControl fullWidth>
-              <Select
-                labelId="chose-chain"
-                value={selectedChain}
-                onChange={handleChainChange}
-                sx={{ width: "190px", height: 40, borderRadius: "25px", pl: 2 }}
-                color="primary"
-              >
-                {chainsIds.map((item) => (
-                  <MenuItem key={`chain-id-${item.id}`} value={item.id}>
-                    {/* <img
-                      height={10}
-                      width={10}
-                      src={item.logo}
-                      alt={item.name}
-                    /> */}
-
-                    {item.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Stack>
-
-          <Stack direction={"column"} spacing={2} sx={{ width: "45vh" }}>
-            <Typography variant="h6">Paste address</Typography>
-            <FormControl fullWidth sx={{ m: 1 }} variant="filled">
-              <Input
-                id="standard-adornment-amount"
-                fullWidth
-                onChange={handleAddress}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={sendTokenToApi}
-                      color="secondary"
-                    >
-                      <SearchIcon />
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-          </Stack>
-        </Stack>
-      </Grid>
+      <Inputs setTokenHolders={setTokenHolders} setTokenData={setTokenData} />
       <Grid
         container
         sx={{ justifyContent: "center", alignItems: "center", mt: 3 }}
@@ -170,7 +56,9 @@ function App() {
               alignItems: "center",
             }}
           >
-            <HoldersTable holders={tokenHolders} theme={theme} />
+            {tokenHolders.length ? (
+              <HoldersTable holders={tokenHolders} theme={theme} />
+            ) : null}
           </Box>
         </Stack>
       </Grid>
---
+++ src/sections/Inputs.tsx
@@ -0,0 +1,131 @@
+// import dependencies
+import { useState } from "react";
+import {
+  Grid,
+  Stack,
+  Typography,
+  FormControl,
+  Select,
+  MenuItem,
+  Input,
+  InputAdornment,
+  IconButton,
+  SelectChangeEvent,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+// import constants
+import { chainsIds } from "../constants/chains";
+import { apiKey } from "../constants/api";
+
+// import types
+import { HolderAddress, TokenData } from "../types/types";
+
+interface InputProps {
+  setTokenHolders: React.Dispatch<React.SetStateAction<HolderAddress[]>>;
+  setTokenData: React.Dispatch<
+    React.SetStateAction<TokenData | null | undefined>
+  >;
+}
+
+const Inputs = ({ setTokenHolders, setTokenData }: InputProps) => {
+  const [selectedChain, setSelectedChain] = useState("1");
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChainChange = (event: SelectChangeEvent) => {
+    setSelectedChain(event.target.value);
+  };
+
+  const handleAddress = (event: any) => {
+    setInputValue(event.target.value);
+  };
+
+  const sendTokenToApi = () => {
+    fetch(
+      `https://api.chainbase.online/v1/token/top-holders?chain_id=${selectedChain}&contract_address=${inputValue}&page=1&limit=100`,
+      {
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+          accept: "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setTokenHolders(data.data))
+      .catch((error) => console.error(error));
+
+    fetch(
+      `https://api.chainbase.online/v1/token/metadata?chain_id=${selectedChain}&contract_address=${inputValue}`,
+      {
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setTokenData(data.data))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <Grid container sx={{ mt: 5 }}>
+      <Stack
+        sx={{ width: "200vh" }}
+        direction={"row"}
+        justifyContent={"space-around"}
+      >
+        <Stack direction={"column"} spacing={2}>
+          <Typography variant="h6">Choose chain:</Typography>
+          <FormControl fullWidth>
+            <Select
+              labelId="chose-chain"
+              value={selectedChain}
+              onChange={handleChainChange}
+              sx={{ width: "190px", height: 40, borderRadius: "25px", pl: 2 }}
+              color="primary"
+            >
+              {chainsIds.map((item) => (
+                <MenuItem key={`chain-id-${item.id}`} value={item.id}>
+                  {/* <img
+                      height={10}
+                      width={10}
+                      src={item.logo}
+                      alt={item.name}
+                    /> */}
+
+                  {item.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Stack>
+
+        <Stack direction={"column"} spacing={2} sx={{ width: "45vh" }}>
+          <Typography variant="h6">Paste address</Typography>
+          <FormControl fullWidth sx={{ m: 1 }} variant="filled">
+            <Input
+              id="standard-adornment-amount"
+              fullWidth
+              onChange={handleAddress}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={sendTokenToApi}
+                    color="secondary"
+                  >
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Stack>
+      </Stack>
+    </Grid>
+  );
+};
+
+export default Inputs;
